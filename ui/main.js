0a13af899d5957861a6aa0addaaa94e2eeb90955
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2017-08-06 17:34:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,7 @@ button.onclick=function(){
     request.send(null);
 };
 //submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var request=new XMLHttpRequest();
@@ -37,6 +36,8 @@ submit.onclick=function(){
         }
         
     };
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
     request.open('GET','http://kartik20393.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     
