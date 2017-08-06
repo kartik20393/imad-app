324602a44ff16e39a977a3db7b2cb1de3f874905
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2017-08-06 16:05:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+//counter code
+var button=doccument.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+   counter=counter+1;
+   var span=doccument.getElementById('count');
+   span.innerHTML=counter.toString();
+};
