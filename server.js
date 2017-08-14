f9de7d9de1ed6dd42984ee4a77bd25711d2bdc78
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2017-08-15 00:13:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/test-db', function (req, res) {
     pool.query('SELECT * FROM test',function(err,result){
         if(err)
         {
-            res.status(500),send(err.toString());
+            res.status(500).send(err.toString());
         }
         else
         {
@@ -115,7 +115,7 @@ app.get('/articles/:articleName', function (req, res) {
     pool.query("SELECT * FROM article WHERE title="+req.params.articleName,function(err,result){
         if(err)
         {
-            res.status(500),send(err.toString());
+            res.status(500).send(err.toString());
         }
         else
         {
