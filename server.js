ca8c8e8ee33e96a1bedf6653ff1361045b4c556e
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2017-08-15 00:07:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,9 +110,28 @@ app.get('/submit-name', function (req, res) {
   res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+    
+    pool.query("SELECT * FROM article WHERE title="+req.params.articleName,function(err,result){
+        if(err)
+        {
+            res.status(500),send(err.toString());
+        }
+        else
+        {
+            if(result.rows.length===0)
+            {
+                res.status(404),send(err.toString());
+            }
+            else
+            {
+                var articledata=result.rows[0];
+                res.send(createTemplate(articledata));
+            }
+            
+        }
+    });
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
