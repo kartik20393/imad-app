1a1ffec6bf90e64532491dd243486029c69608f2
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2017-08-21 16:27:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/', function (req, res) {
 function hash(input,salt)
 {
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');
+    return ['pbkdf2Sync','10000',salt,hashed.toString('hex')].join('$');
 }
 app.get('/hash/:input', function (req, res) {
   var hashedstring=hash(req.params.input,'this-is-some-random-string');
