82812683bfc5e50d4bfe4cb9c9006f9c3665cfa2
Kartik Badarinarayana <30652456+kartik20393@users.noreply.github.com>
2017-08-21 16:51:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
+var bodyParser=require('body-parser');
 var config={
     user:'kartik20393',
     database:'kartik20393',
@@ -13,6 +14,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 
 function createTemplate(data)
@@ -67,6 +69,26 @@ app.get('/hash/:input', function (req, res) {
   var hashedstring=hash(req.params.input,'this-is-some-random-string');
   res.send(hashedstring);
 });
+
+
+app.post('/create-user', function (req, res) {
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.getRandomBytes(128).toString('hex');
+  var dbstring=hash(password,salt);
+  pool.query('INSERT INTO"user"(username,password) VALUES($1,$2)',[username,dbstring],function(err,result){
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send('user succesfully created:'+username);
+        }
+    });
+  
+});
+
 
 var pool=new Pool(config);
 app.get('/test-db', function (req, res) {
